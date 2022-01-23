53c8f8bf24c7230e528e6d06b430efeb081752c8
Chintan Shah <chintan@wohlig.com>
2022-01-23 16:27:52 +0530

    changes

--- services/User.js
+++ services/User.js
@@ -1,8 +1,8 @@
 export default {
   sayHello(call, callback) {
-    callback(null, { message: 'Hello ' + call.request.name });
+    callback(null, { message: 'Hello this is User, ' + call.request.name });
   },
   sayHelloAgain(call, callback) {
-    callback(null, { message: 'Hello ' + call.request.name });
+    callback(null, { message: 'Hello this is User, ' + call.request.name });
   },
 };
