963415c268654e84d4b1a983085596e891927c40
Chintan Shah <chintan@wohlig.com>
2022-01-23 16:30:16 +0530

    chanegs

---
+++ services/Teacher.js
@@ -0,0 +1,8 @@
+export default {
+  sayHello(call, callback) {
+    callback(null, { message: 'Hello this is User, ' + call.request.name });
+  },
+  sayHelloAgain(call, callback) {
+    callback(null, { message: 'Hello this is User, ' + call.request.name });
+  },
+};
