fb2a2a2da972b23614352beec8c9e7c6fe90ae81
Chintan Shah <chintan@wohlig.com>
2022-02-12 20:17:57 +0530

    changes

--- services/Student.js
+++ services/Student.js
@@ -1,9 +1,9 @@
 /* eslint-disable node/no-unsupported-features/es-syntax */
 export default {
   sayHello(call, callback) {
-    callback(null, { message: `Hello ${  call.request.name}` });
+    callback(null, { message: `Hello ${call.request.name}` });
   },
   sayHelloAgain(call, callback) {
-    callback(null, { message: `Hello ${  call.request.name}` });
+    callback(null, { message: `Hello ${call.request.name}` });
   },
 };
